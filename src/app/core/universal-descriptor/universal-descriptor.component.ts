32cbdac59a996ca4522f93e7aa58e2260d3cd8f6
Dominic Hiland <dominic.dev.email@gmail.com>
2024-03-27 12:39:48 -0500

    Improved positioning of #info-access and added a loading indicator to the UniversalDescriptor iframe

--- src/app/core/universal-descriptor/universal-descriptor.component.ts
+++ src/app/core/universal-descriptor/universal-descriptor.component.ts
@@ -3,6 +3,7 @@ import {MatDialogContent} from "@angular/material/dialog";
 import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
 import {MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
+import {MatProgressBar} from "@angular/material/progress-bar";
 
 @Component({
   standalone: true,
@@ -13,15 +14,31 @@ import {MatIcon} from "@angular/material/icon";
     MatCardContent,
     MatCardTitle,
     MatIconButton,
-    MatIcon
+    MatIcon,
+    MatProgressBar
   ],
   templateUrl: 'universal-descriptor.component.html',
   styleUrl: 'universal-descriptor.component.scss',
 })
-export class UniversalDescriptorComponent {
+export class UniversalDescriptorComponent implements OnInit {
 
   @Output() onClickClose: EventEmitter<any> = new EventEmitter<any>()
 
+  frameLoading: boolean = false
+
   readonly contentId: string = 'universal-descriptor-iframe'
+
+  ngOnInit(): void {
+    this.frameLoading = true
+    const el = document.getElementById('iframe-container')
+    const iframe = document.createElement('iframe')
+    iframe.onload = () => this.frameLoading = false
+    iframe.id = this.contentId
+    iframe.src = 'https://master--study-together-universal-descriptor.netlify.app/'
+    iframe.style.border = 'none'
+    iframe.style.width = 'calc(100% - 2px)'
+    iframe.style.height = '100%'
+    el?.appendChild(iframe)
+  }
 
 }
